49bcd856002c582509f6a96c2ce6180ddb615a4e
David Poirier-Quinot <davipoir@gmail.com>
2020-07-13 22:35:04 +0200

    replaced line break \n with windows \r\n equivalent (expected by CATT)

--- export.py
+++ export.py
@@ -90,37 +90,37 @@ def catt_ascii_write(filepath, vertices, faces, obj, triangulate, fileName):
 
         # Catt related header
         header = ";" + fileName + ".GEO"
-        fw('%s\n' % header)
+        fw('%s\r\n' % header)
         header = ";PROJECT="
-        fw('%s\n\n' % header)
+        fw('%s\r\n\r\n' % header)
 
         # Blender Add-on related header
         header = ";FILE GENERATED VIA BLENDER CATT EXPORT ADD-ON"
-        fw('%s\n' % header)
+        fw('%s\r\n' % header)
         blendFilename = os.path.splitext(os.path.split(bpy.data.filepath)[1])[0]
         if not blendFilename:
             blendFilename = 'floating file (unsaved)'
         else:
             blendFilename = blendFilename + ".blend"
         header = ";BASED ON .BLEND FILE: " + blendFilename
-        fw('%s\n\n' % header)
+        fw('%s\r\n\r\n' % header)
 
         # write material(s)
         header = ""
         for mat in obj.data.materials:
-            tmp = "abs {0} = <{1} {2} {3} {4} {5} {6} : {7} {8} > L < {9} {10} {11} {12} {13} {14} : {15} {16}> {{{17} {18} {19}}} \n".format(mat.name,int(mat['abs_0']),int(mat['abs_1']),int(mat['abs_2']),int(mat['abs_3']),int(mat['abs_4']),int(mat['abs_5']),int(mat['abs_6']),int(mat['abs_7']),int(mat['dif_0']),int(mat['dif_1']),int(mat['dif_2']),int(mat['dif_3']),int(mat['dif_4']),int(mat['dif_5']),int(mat['dif_6']),int(mat['dif_7']),int(100*mat.diffuse_color[0]),int(100*mat.diffuse_color[1]),int(100*mat.diffuse_color[2]))
+            tmp = "abs {0} = <{1} {2} {3} {4} {5} {6} : {7} {8} > L < {9} {10} {11} {12} {13} {14} : {15} {16}> {{{17} {18} {19}}} \r\n".format(mat.name,int(mat['abs_0']),int(mat['abs_1']),int(mat['abs_2']),int(mat['abs_3']),int(mat['abs_4']),int(mat['abs_5']),int(mat['abs_6']),int(mat['abs_7']),int(mat['dif_0']),int(mat['dif_1']),int(mat['dif_2']),int(mat['dif_3']),int(mat['dif_4']),int(mat['dif_5']),int(mat['dif_6']),int(mat['dif_7']),int(100*mat.diffuse_color[0]),int(100*mat.diffuse_color[1]),int(100*mat.diffuse_color[2]))
             header = header + tmp
-        fw('%s\n' % header)
+        fw('%s\r\n' % header)
 
         # write corners (vertices)
         header = "CORNERS"
-        fw('%s\n\n' % header)
+        fw('%s\r\n\r\n' % header)
         for vertice in vertices:
-            fw("{0} {1:.2f} {2:.2f} {3:.2f} \n".format(vertice.index, vertice.co[0], vertice.co[1], vertice.co[2]) )
+            fw("{0} {1:.2f} {2:.2f} {3:.2f} \r\n".format(vertice.index, vertice.co[0], vertice.co[1], vertice.co[2]) )
 
         # write planes (faces)
         header = "PLANES"
-        fw('\n%s\n\n' % header)
+        fw('\r\n%s\r\n\r\n' % header)
         faceID = 1
         for face in faces:
             planeName = 'wall' # default plane name set in Catt
@@ -132,16 +132,16 @@ def catt_ascii_write(filepath, vertices, faces, obj, triangulate, fileName):
 
             if len(face.vertices) == 4:
                 if not triangulate:
-                    fw("[ {0} {1} / {2} {3} {4} {5} / {6} ]\n".format(faceID, planeName, face.vertices[0], face.vertices[1], face.vertices[2], face.vertices[3], planeMaterial) )
+                    fw("[ {0} {1} / {2} {3} {4} {5} / {6} ]\r\n".format(faceID, planeName, face.vertices[0], face.vertices[1], face.vertices[2], face.vertices[3], planeMaterial) )
                     faceID = faceID + 1
                 else: # quads to tris to make sure all vertices of a plane are in the same plane in catt
-                    fw("[ {0} {1} / {2} {3} {4} / {5} ]\n".format(faceID, planeName, face.vertices[0], face.vertices[1], face.vertices[2], planeMaterial) )
+                    fw("[ {0} {1} / {2} {3} {4} / {5} ]\r\n".format(faceID, planeName, face.vertices[0], face.vertices[1], face.vertices[2], planeMaterial) )
                     faceID = faceID + 1
-                    fw("[ {0} {1} / {2} {3} {4} / {5} ]\n".format(faceID, planeName, face.vertices[2], face.vertices[3], face.vertices[0], planeMaterial) )
+                    fw("[ {0} {1} / {2} {3} {4} / {5} ]\r\n".format(faceID, planeName, face.vertices[2], face.vertices[3], face.vertices[0], planeMaterial) )
                     faceID = faceID + 1
 
             elif len(face.vertices) == 3:
-                fw("[ {0} {1} / {2} {3} {4} / {5} ]\n".format(faceID, planeName, face.vertices[0], face.vertices[1], face.vertices[2], planeMaterial) )
+                fw("[ {0} {1} / {2} {3} {4} / {5} ]\r\n".format(faceID, planeName, face.vertices[0], face.vertices[1], face.vertices[2], planeMaterial) )
                 faceID = faceID + 1
             else:
                 print("error: exporter only supports tris and quads, face {} has {} vertices".format(faceID, len(face.vertices)))
